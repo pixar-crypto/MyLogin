7fc83c67cb5834c88c222da7442b6de70fa4c92f
Syahrul Ramadhan <0320200028@polman.astra.ac.id>
2021-05-25 14:57:43 +0700

    nambah label di form1

--- MyLogin/Form1.Designer.cs
+++ MyLogin/Form1.Designer.cs
@@ -105,9 +105,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(117, 284);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.Size = new System.Drawing.Size(111, 20);
             this.label4.TabIndex = 6;
-            this.label4.Text = "syahrul keren";
+            this.label4.Text = "ssyahrul keren";
             // 
             // Form1
             // 
